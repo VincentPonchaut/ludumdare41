90b678e52ecba777c67246c15f975207a4a3e3c2
VincentPonchaut <vincent.ponchaut@hotmail.fr>
2018-04-21 19:17:53 +0200

    WIP LevelManager

---
+++ test/Assets/Script/LevelManager.cs
@@ -0,0 +1,69 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LevelManager : MonoBehaviour
+{
+    private int LevelCount; // number of level achieved
+    private string CurrentLevel;
+    private int CurrentLevelMaxEnemies;
+
+    private int RandomSceneIndexMin = 0;
+    private int RandomSceneIndexMax = -1;
+
+
+    #region Methods
+
+    public void ShowStartMenu()
+    {
+
+    }
+
+    public void AttemptExit(/*Vector2 exitPosition*/)
+    {
+        // TODO
+        // Unload current level
+        // Load Question level
+    }
+
+    private void RestartLevel()
+    {
+
+    }
+
+    private void NextLevel()
+    {
+
+    }
+
+    private void LoadLevel(string levelName)
+    {
+        // TODO
+        // Load level
+        // Spawn player
+        // Spawn enemies
+        SceneManager.LoadScene(levelName, LoadSceneMode.Additive);
+    }
+
+    private void SpawnRandomEnemies()
+    {
+
+    }
+
+    #endregion
+
+    #region UnityBehavior
+    // Use this for initialization
+    void Start()
+    {
+        // Show start menu
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+    #endregion
+}

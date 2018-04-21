5edb76e631737b3e46b79ec635c6da042430d665
VincentPonchaut <vincent.ponchaut@hotmail.fr>
2018-04-21 17:43:58 +0200

    WIP damage handling

---
+++ test/Assets/Script/EnemyController.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Character))]
+public class EnemyController : MonoBehaviour
+{
+    Character character;
+
+    // Use this for initialization
+    void Start()
+    {
+        character = GetComponent<Character>();
+    }
+	
+	// Update is called once per frame
+	void Update ()
+    {
+#if UNITY_EDITOR
+        // Debug only
+        if (Input.GetKeyDown("space"))
+            character.Throw();
+#endif
+    }
+}
--- test/Assets/ThrowableItem.cs
+++ test/Assets/ThrowableItem.cs
@@ -5,12 +5,18 @@ using UnityEngine;
 public class ThrowableItem : MonoBehaviour
 {
     public GameObject hitPrefab;
+    public Character Thrower;
 
     void OnCollisionEnter2D(Collision2D collision)
     {
-        if (collision.gameObject.GetComponent<Character>())
+        if (this.Thrower != null && 
+            collision.gameObject.GetComponent<Character>())
         {
-            Debug.Log("Apply damage");
+            Character hitCharacter = collision.gameObject.GetComponent<Character>();
+            if (hitCharacter == this.Thrower)
+                return;
+
+            hitCharacter.ApplyDamageFrom(this.Thrower, this);
         }
         else if (collision.gameObject.GetComponent<ThrowableItem>())
         {
@@ -18,11 +24,11 @@ public class ThrowableItem : MonoBehaviour
             //Quaternion rot = Quaternion.FromToRotation(Vector3.up, contact.normal);
             //Vector3 pos = contact.point;
 
-            GameObject hitObject = Instantiate(hitPrefab, transform.localPosition, transform.localRotation);
+            GameObject hitObject = Instantiate(hitPrefab, transform.localPosition, transform.localRotation) as GameObject; 
             Destroy(collision.gameObject);
             Destroy(hitObject, 0.1f);
         }
 
-        Destroy(this);
+        Destroy(this.gameObject);
     }
 }

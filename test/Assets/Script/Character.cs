efd6d90808eee92effcdaa05c6dbb8ee08d25073
Thomas TECHENE <techene.thomas@gmail.com>
2018-04-21 14:57:22 +0200

    First level test + character moving

---
+++ test/Assets/Script/Character.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Character : MonoBehaviour {
+
+    public int speed = 10;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+
+        float moveHorizontal = Input.GetAxis("Horizontal");
+        float moveVertical = Input.GetAxis("Vertical");
+
+        Vector3 movement = new Vector3(moveHorizontal, moveVertical);
+        GetComponent<Rigidbody2D>().AddForce(movement * speed);
+	}
+}

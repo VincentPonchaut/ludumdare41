9f98ba2db5af2f126ba3ec32a504c4c087117918
VincentPonchaut <vincent.ponchaut@hotmail.fr>
2018-04-22 16:23:17 +0200

    Added Right and wrong sounds to questions

--- test/Assets/AudioManager.cs
+++ test/Assets/AudioManager.cs
@@ -7,6 +7,14 @@ public class AudioManager : MonoBehaviour
     private AudioSource bgmSource;
     public float BgmVolume = 0.5f;
 
+    private AudioSource soundEffectSource;
+    public float SoundEffectVolume = 0.5f;
+
+    // Effects
+    public AudioClip CorrectAnswerSound;
+    public AudioClip WrongAnswerSound;
+
+    #region Methods
     public void PlayBackgroundMusic(AudioClip clip)
     {
         bgmSource.clip = clip;
@@ -14,10 +22,33 @@ public class AudioManager : MonoBehaviour
         bgmSource.Play();
     }
 
+    public void PlaySoundEffect(AudioClip clip)
+    {
+        soundEffectSource.clip = clip;
+        soundEffectSource.volume = SoundEffectVolume;
+        soundEffectSource.Play();
+    }
+
+    public void PlayCorrectAnswerSound()
+    {
+        PlaySoundEffect(CorrectAnswerSound);
+    }
+
+    public void PlayWrongAnswerSound()
+    {
+        PlaySoundEffect(WrongAnswerSound);
+    }
+    #endregion
+
+    #region Unity Methods
     // Use this for initialization
     void Start()
     {
-        bgmSource = GetComponent<AudioSource>();
+        bgmSource = GetComponents<AudioSource>()[0];
         bgmSource.loop = true;
+
+        soundEffectSource = GetComponents<AudioSource>()[1];
+        soundEffectSource.loop = false;
     }
+    #endregion
 }

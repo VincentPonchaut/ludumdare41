47547b8b6d7d395fb2d4a7cbb3970b96ab66313f
VincentPonchaut <vincent.ponchaut@hotmail.fr>
2018-04-22 16:07:41 +0200

    Added musics and welcome screen Bugfix: Arrows still updated GetAxis function

---
+++ test/Assets/WelcomeScene.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class WelcomeScene : MonoBehaviour
+{
+    public void StartGame(bool asVegetarian)
+    {
+        // TODO: handle choice
+        SceneManager.LoadScene("Init");
+    }
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
